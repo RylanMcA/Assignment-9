81bb76718e77f292bcc3329573bc931d2a5ac160
RylanMcA <55506412+RylanMcA@users.noreply.github.com>
2019-12-01 17:15:24 -0600

    Setting up files

---
+++ Assignment9.py
@@ -0,0 +1,10 @@
+import sys
+import operator
+
+#argv[0] is the name of this python file
+cmdStoryFile = sys.argv[1]
+cmdSkipFile = sys.argv[2]
+
+erasures = ['\n','\t','.','?','!',',',';',':','\'','\"']
+
+input()
